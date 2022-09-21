f2e10762c518aec3bc9503a5b4aa2a31fdbff13c
onuce1 <onuca001@osloskolen.no>
2022-09-21 10:57:42 +0200

    commit

--- nyheter.js
+++ nyheter.js
@@ -9,7 +9,7 @@ sport = function() {
     content.innerHTML = "Portugal-kaptein Cristiano Ronaldo (37) garanterer at hans tid på landslaget ikke er over med det første. Under en galla for det portugisiske fotballforbundet sa stjernen at han regnet med å delta i VM senere i år og EM i 2024."+
     " Jeg håper å være en del av forbundet i noen år til. Jeg føler meg fortsatt motivert, ambisjonene mine er høye. Min vei på landslaget er ikke ferdig. Vi har mange ungdommer med kvalitet. Jeg skal være med i VM og jeg ønsker å være med i EM, sier Ronaldo ifølge den portugisiske avisen Renascenca."+
     "Ronaldo har startet de fleste av kampene til Manchester United på benken hittil i sesongen."+
-    "<img src='ronaldo.png'>";
+    "<img src='ronaldo.jpg'>";
 }
 
 kultur = function() {
