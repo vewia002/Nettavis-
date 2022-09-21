7e61d515b3221c81a276723de912f407affc4a22
vewia002 <vewia002@osloskolen.no>
2022-09-21 10:59:51 +0200

    commit

--- nyheter.js
+++ nyheter.js
@@ -13,6 +13,19 @@ kultur = function() {
 }
 
 bakka = function() {
-    content.innerHTML = "";
+    content.innerHTML = "Sykkellaget knuser alle standarder!"+
+    "The mystery of Bike isn't a problem to solve, but a reality to experience, sier det nye suksessrike lagets leder"+
+
+    "<p>Sykkellaget er et elevlag som har sjokkert mange i det siste. Den høye medlemsveksten beviser for mange,"+ 
+     "ifølge kilder som ønsker å være anonyme, at det nye elevlaget appellerer til en slags sovende kraft dypt inni"+ 
+     "underbevisstheten til mange av elvebakkens elever."+ 
+     "-Sykkelaget har gitt meg en mulighet til å omfaven deler av min psyke som jeg ikke visste eksisterte. Gjennom"+
+     "sykkelen får den spirituelle delen av meg en mulighet til å utfolde seg. Ved å kombinere den sovende og våkne delen av"+ 
+     "min bevissthet transformeres hele tilværelsen min, og sykkelen oppsluker meg til den grad jeg BLIR selve Sykkelen."+
+     "Dette sier en av våre anonyme kilder til vår reporter på stedet."+
+     "Om spørsmål på hvorfor så mange melder seg inn i elevlaget svarer deres leder følgende:"+ 
+    "-Sykkellaget er et elelvlag som er åpent alle. Hvis du ikke liker Sykkel lærer vi deg å like Sykkel. Dessuten kan vi"+ 
+    " love en total innlemmelse i Sykkellagets zeitgeist som ikke kan sammenlignes med noe noen andre elevlag kan tilby"+
+    </p>";
 }
 
