ba0f5c3c2c829fffb87a9e2a5c458ef97a546505
auina001 <113507412+auina001@users.noreply.github.com>
2022-09-21 10:30:53 +0200

    commit on 'dev_august'

--- nyheter.js
+++ nyheter.js
@@ -1,16 +1,18 @@
-news = function() {
+const content = document.getElementById("content");
 
+news = function() {
+    content.innnerHTML = "";
 }
 
 sport = function() {
-
+    content.innerHTML = "";
 }
 
 kultur = function() {
-
+    content.innerHTML = "";
 }
 
 bakka = function() {
-    
+    content.innerHTML = "";
 }
 
